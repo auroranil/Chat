ce124fe73e202ae41edfd9c8ae978493e22aa3f2
Saurabh Joshi <sauj123@gmail.com>
2015-12-30 22:51:32 +1100

    switched one line setup command from wget to curl

--- setup_server.sh
+++ setup_server.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# Delete the script itself
-rm -- "$0"
-
+echo Running "git clone https://github.com/jos0003/Chat.git"
 git clone https://github.com/jos0003/Chat.git
+echo Running "cd Chat"
 cd Chat
 
 # Install these dependencies
+echo Running "sudo apt-get install sqlite python-pip"
 sudo apt-get install sqlite python-pip
+echo Running "sudo pip install flask bcrypt flask-bcrypt flask-socketio sqlalchemy flask-sqlalchemy"
 sudo pip install flask bcrypt flask-bcrypt flask-socketio sqlalchemy flask-sqlalchemy
 
 # Run server on port 5000 as specified in the second argument.
 # This will create config.py, which will store the configuration
 # settings in a function.
+echo Running "python server_app 5000"
 python server_app 5000
