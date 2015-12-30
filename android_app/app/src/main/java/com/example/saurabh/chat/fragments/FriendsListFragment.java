85347e784597e4cefc8ef596151cefcf69af416e
Saurabh Joshi <sauj123@gmail.com>
2015-12-30 16:35:03 +1100

    Changed "no friends" message; now using statusLayout in FriendsListFragment

--- android_app/app/src/main/java/com/example/saurabh/chat/fragments/FriendsListFragment.java
+++ android_app/app/src/main/java/com/example/saurabh/chat/fragments/FriendsListFragment.java
@@ -5,12 +5,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.saurabh.chat.R;
+import com.example.saurabh.chat.layouts.StatusLayout;
 
 public class FriendsListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+        View v = inflater.inflate(R.layout.fragment_friends, container, false);
+
+        StatusLayout statusLayout = (StatusLayout) v.findViewById(R.id.layout_status_friendlist);
+        statusLayout.setError("You haven't added any friends yet.");
+
+        ListView friendsListView = (ListView) v.findViewById(R.id.listView_friends);
+        friendsListView.setVisibility(View.GONE);
+
+        return v;
     }
 }
